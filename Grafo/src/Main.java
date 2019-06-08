9371616d2e0db8804d38b7f6326b517cc489e377
lrdantas <luisrodrigo_27@yahoo.com.br>
2019-06-08 01:12:08 -0300

    add funcionalidade de ler arquivo de arestas e vertices

---
+++ Grafo/src/Main.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Luis
+ */
+
+import entrada.LerArquivo;
+import grafo.Aresta;
+import grafo.Dijkstra;
+import grafo.Grafo;
+import grafo.Vertice;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          Grafo grafo;
+        
+        LerArquivo arquivo = new LerArquivo("vertices");
+        arquivo.run();
+        List lista_vertices = arquivo.getLista();
+        grafo = new Grafo(arquivo.getNum_vertices());
+        
+        arquivo = new LerArquivo("arestas");
+        arquivo.run();
+        List lista_arestas = arquivo.getLista();
+        Vertice v;
+        Aresta a;
+        String linha;
+        String[] valores;
+        StringBuilder strName = new StringBuilder();
+        strName.append("v");
+        int key_a;
+        int key_b;
+        Vertice va;
+        Vertice vb;
+        
+        //Inserir vertices no grafo de custo.
+        for(int i = 0; i < lista_vertices.size() ; i++){
+            linha = (String) lista_vertices.get(i);
+            valores = linha.split(" ");
+            strName = new StringBuilder();
+            strName.append("v").append(valores[2]);
+            
+            v = new Vertice(Integer.parseInt(valores[2]), strName);
+            v.setLatitude(valores[0]);
+            v.setLongitude(valores[1]);
+            grafo.inserir_vertice(v);            
+        }
+        
+         //Inserir arestas no grafo de custo.
+         for(int i = 0; i < lista_arestas.size() ; i++){
+              linha = (String) lista_arestas.get(i);
+              valores = linha.split(" ");
+              key_a = Integer.parseInt(valores[0]);
+              va = (Vertice) grafo.getVertices().get(key_a);
+              key_b = Integer.parseInt(valores[1]);
+              vb = (Vertice) grafo.getVertices().get(key_b);
+              
+              a = new Aresta(Integer.parseInt(valores[2]), va , vb);
+              grafo.inserir_aresta(a);
+              a = new Aresta(Integer.parseInt(valores[2]), vb , va);
+              grafo.inserir_aresta(a);
+         }
+         
+         //grafo.print();
+         
+         Dijkstra dijkstra = new Dijkstra(grafo);
+         
+         Object[] vertices = grafo.getVertices().toArray();
+         
+         
+         int peso;
+         Object[] dist;
+         String valor;
+         
+         //Rodando dijkstra
+         for(int i = 0; i < vertices.length; i++){  
+             peso = 0;
+             dijkstra.dijkstra((Vertice) vertices[i]);
+             dist = dijkstra.getDists();
+              for (Object dist1 : dist) {
+                  valor = dist1.toString();
+                  //System.out.println(valor);
+                  peso = peso + Integer.parseInt(valor);
+              }
+             System.out.println("vertice " + i + " com peso: " + peso);
+         }
+
+        
+    }
+    
+}
---
+++ Grafo/src/entrada/LerArquivo.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entrada;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Luis
+ */
+public class LerArquivo {
+    
+    private Scanner x;
+    private String entrada;
+    private final String caminho = "src/entrada/" + entrada;
+    private int num_vertices = 0;
+   
+    private List lista = new ArrayList();
+
+    public LerArquivo(String entrada) {
+        this.entrada = entrada;
+    }
+ 
+    public void abrirArquivo(String entrada){
+        
+        try {
+            x = new Scanner(new File("src/entrada/" + entrada));
+        } catch (Exception e) {
+            System.out.println("Arquivo não encontrado");
+        }
+ 
+    }
+    
+    public  void lerArquivo(){
+        abrirArquivo(entrada);
+        while (this.x.hasNext()) {
+           num_vertices++;
+           this.lista.add(this.x.nextLine());
+     
+        }
+    }
+    
+    public void fechar(){
+        x.close();
+    }
+    
+    public void run(){
+        abrirArquivo(entrada);
+        lerArquivo();
+        fechar();
+    }
+
+    public Scanner getX() {
+        return x;
+    }
+
+    public List getLista() {
+        return lista;
+    }
+
+    public int getNum_vertices() {
+        return num_vertices;
+    }
+    
+    
+    
+    
+    
+    
+
+}
